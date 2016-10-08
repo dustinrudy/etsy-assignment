49947a4e5740af9b82db54adeb56ff5cfba106ff
Dustin Rudy <dustinrudy@Dustins-MacBook-Pro.local>
2016-10-08 16:51:52 -0700

    Displayed search results from json file still need to fix styling

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 
+var domArray = []
+
+
 $.getJSON( "https://api.etsy.com/v2/listings/active?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-  	console.log(key, val)
-  });
+	console.log(data)
+	putInDom(data)
 });
+
+
+function putInDom(data) {
+   var ourData = data.results.map(function(item){
+       return {
+           price: item.price,
+           link: item.url,
+           title: item.title,
+           img: item.Images[0].url_570xN
+       }
+   })
+
+   var htmlStr = "";
+   ourData.forEach(function(item){
+       htmlStr += `
+       <div class="pics">
+           <img src="${item.img}" />
+           <p id="name"> ${item.title} </p>
+           <p id="url"> ${item.url} </p>
+           <p id="price"> ${item.price} </p>
+           
+       </div>
+       `
+   })
+
+   console.log(htmlStr)
+
+   $("#allpics").html(htmlStr)
+}
 
 
 
